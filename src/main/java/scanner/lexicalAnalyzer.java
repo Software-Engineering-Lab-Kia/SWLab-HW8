cb461f3ac00afc45bba9fce51e912065e137fe0a
Kian <oxfvard@gmail.com>
2024-06-04 02:54:41 +0330

    Replace Temp With Query

--- src/main/java/scanner/lexicalAnalyzer.java
+++ src/main/java/scanner/lexicalAnalyzer.java
@@ -15,28 +15,28 @@ public class lexicalAnalyzer {
         while (sc.hasNext()) {
             input.append(sc.nextLine());
         }
-        StringBuilder tokenPattern = new StringBuilder();
-        for (Type tokenType : Type.values())
-            tokenPattern.append(String.format("|(?<%s>%s)", tokenType.name(), tokenType.pattern));
-        Pattern expression = Pattern.compile(tokenPattern.substring(1));
+        Pattern expression = Pattern.compile(buildTokenPattern());
         matcher = expression.matcher(input.toString());
     }
 
-    public Token getNextToken() {
+    private String buildTokenPattern() {
+        StringBuilder tokenPattern = new StringBuilder();
+        for (Type tokenType : Type.values())
+            tokenPattern.append(String.format("|(?<%s>%s)", tokenType.name(), tokenType.pattern));
+        return tokenPattern.substring(1);
+    }
 
+    public Token getNextToken() {
         while (matcher.find()) {
             for (Type t : Type.values()) {
-
                 if (matcher.group(t.name()) != null) {
                     if (matcher.group(Type.COMMENT.name()) != null) {
                         break;
-
                     }
                     if (matcher.group(Type.ErrorID.name()) != null) {
                         ErrorHandler.printError("The id must start with character");
                         break;
                     }
-
                     return new Token(t, matcher.group(t.name()));
                 }
             }
